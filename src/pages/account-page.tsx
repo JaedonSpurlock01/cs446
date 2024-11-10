55b0761dbfd870019ba2ad0e15b66ba19b7bf331
jaedonspurlock01 <jaedonaspurlock@outlook.com>
2024-11-09 20:03:37 -0800

    refactor: Added todo comments

--- src/pages/account-page.tsx
+++ src/pages/account-page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function AccountPage() {
-    // TODO: Make sure to check user is logged in before accessing
+  // TODO: Make sure to check user is logged in before accessing
   return (
     <div>AccountPage</div>
   )
